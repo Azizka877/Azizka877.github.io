84818e7362bbaf84f48ad0da6794ad958ec88d00
Abdoul Aziz Ka <abdoulaziz.ka877@gmail.com>
2023-11-14 16:25:43 +0000

    mon portefolio astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Azizka877.github.io',
-    base: '/Portefolio',
+   
 });
